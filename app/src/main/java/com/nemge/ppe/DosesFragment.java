ec5ab80f4b722ec94eae56cbf7e6a8bb486b3f1b
NemGe <geo.nemouchy@gmail.com>
2019-01-09 10:40:34 +0100

    KotlinToJava
    Files modified : from Kotlin to Java

---
+++ app/src/main/java/com/nemge/ppe/DosesFragment.java
@@ -0,0 +1,28 @@
+package com.nemge.ppe;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DosesFragment extends Fragment {
+
+    public DosesFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_doses, container, false);
+        return view;
+    }
+}
