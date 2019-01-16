fe59981c0cd3c872829b7c0c01ce0dd27730cdd7
NemGe <geo.nemouchy@gmail.com>
2019-01-16 13:49:41 +0100

    Add changes
    Admin activity + changes in SQLiteActivity

---
+++ app/src/main/java/com/nemge/ppe/AdminActivity.java
@@ -0,0 +1,21 @@
+package com.nemge.ppe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        this.setSupportActionBar((Toolbar)this.findViewById(R.id.home_toolbar));
+        ActionBar actionbar = this.getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
--- app/src/main/java/com/nemge/ppe/SQLiteDatabasePPE.java
+++ app/src/main/java/com/nemge/ppe/SQLiteDatabasePPE.java
@@ -16,9 +16,7 @@ public class SQLiteDatabasePPE extends SQLiteOpenHelper {
 
     //DOSES TABLE
     public static final String TABLE_DOSES = "doses_table";
-    public static final String COL_ID_USER = "id_user";
-    public static final String COL_HOURS = "hours";
-    public static final String COL_DATE = "date";
+    public static final String COL_DATE = "date_hours";
 
     public SQLiteDatabasePPE(Context context){
         super(context, DATABASE_NAME, null, 1);
@@ -27,7 +25,7 @@ public class SQLiteDatabasePPE extends SQLiteOpenHelper {
     @Override
     public void onCreate(android.database.sqlite.SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_USERS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, PASSWORD TEXT) ");
-        db.execSQL("CREATE TABLE " + TABLE_DOSES + " (ID_USER INTEGER NOT NULL CONSTRAINT COL_ID_USERS REFERENCES TABLE_USERS(COL_ID), HOURS DATETIME, DATE DATETIME) ");
+        db.execSQL("CREATE TABLE " + TABLE_DOSES + " (DATE_HOURS DATETIME) ");
     }
 
     @Override
