d01cb1f2d6884d5fe983dd85b4593d7efd190fae
NemGe <geo.nemouchy@gmail.com>
2019-01-16 15:26:51 +0100

    Nav Settings improved
    Access to Admin Activity from Settings

--- app/src/main/java/com/nemge/ppe/AdminActivity.java
+++ app/src/main/java/com/nemge/ppe/AdminActivity.java
@@ -1,11 +1,20 @@
 package com.nemge.ppe;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AdminActivity extends AppCompatActivity {
+    SQLiteDatabasePPE db;
+    EditText name, password;
+    Button button_view_all, button_clear_all, button_add, button_del;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,38 @@ public class AdminActivity extends AppCompatActivity {
         if (actionbar != null) {
             actionbar.setDisplayHomeAsUpEnabled(true);
         }
+
+        db = new SQLiteDatabasePPE(this);
+        button_view_all = findViewById(R.id.button_view_data);
+        ViewData();
+    }
+
+    public void ViewData(){
+        button_view_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor data = db.getAllData();
+                if(data.getCount()==0){
+                    return;
+                }
+                else{
+                    StringBuffer buffer = new StringBuffer();
+                    while(data.moveToNext()){
+                        buffer.append("ID : " + data.getString(0)+ "\n ");
+                        buffer.append("NAME : " + data.getString(1)+ "\n ");
+                        buffer.append("PASSWORD : " + data.getString(2)+ "\n ");
+                    }
+                    showMessage("Data", buffer.toString());
+                }
+            }
+        });
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
--- app/src/main/java/com/nemge/ppe/SettingsActivity.kt
+++ app/src/main/java/com/nemge/ppe/SettingsActivity.kt
@@ -1,10 +1,15 @@
 package com.nemge.ppe
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v7.app.ActionBar
+import android.view.MenuItem
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,22 @@ class SettingsActivity : AppCompatActivity() {
         actionbar?.apply {
             setDisplayHomeAsUpEnabled(true)
         }
+        navigationView = findViewById(R.id.navigation_settings)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            R.id.menu_settings_admin -> {
+                val intentAdmin = Intent(this, AdminActivity::class.java)
+                startActivity(intentAdmin)
+                return true
+            }
+            else -> {
+            }
+        }
+        return true
     }
 }
 
