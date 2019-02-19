b2735df66298826e24855693a32b4594e09f02c5
Rognetta <aurelio.rognetta@edu.ece.fr>
2019-02-19 15:41:34 +0100

    Data
    Data

--- app/src/main/java/com/nemge/ppe/ChartsFragment.java
+++ app/src/main/java/com/nemge/ppe/ChartsFragment.java
@@ -13,6 +13,18 @@ import com.jjoe64.graphview.series.DataPoint;
 
 public class ChartsFragment extends Fragment {
 
+    public int[] tabJour1 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+    public int[] tabJour2 = new int[]{0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 2, 1, 1, 1, 0, 0, 2, 1, 0};
+    public int[] tabJour3 = new int[]{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
+
+    public int[] tabSemaine1 = new int[]{2, 2, 3, 3, 4, 4, 5};
+    public int[] tabSemaine2 = new int[]{2, 2, 7, 7, 4, 4, 5};
+    public int[] tabSemaine3 = new int[]{5, 5, 7, 7, 4, 4, 2};
+
+    public int[] tabMois1 = new int[]{10,12,14,16};
+    public int[] tabMois2 = new int[]{18,14,14,16};
+    public int[] tabMois3 = new int[]{8,20,14,12};
+
     public ChartsFragment() {
 
     }
