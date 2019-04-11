3c0e6d18cbed20fc634b1cb8a85c2e26d6c92acd
Rognetta <aurelio.rognetta@edu.ece.fr>
2019-04-11 21:31:34 +0200

    Update convert
    Update convert to have several datetimes

--- app/src/main/java/com/nemge/ppe/Test.java
+++ app/src/main/java/com/nemge/ppe/Test.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -26,11 +27,13 @@ public class Test extends AppCompatActivity {
     private static final String FILE_NAME_ONE = "test.txt";
 
     TextView original, changed, result;
-    Button btnconvert;
+    Button btnconvert, btnload, btnsave;
     EditText test;
 
-    String waitingText;
-    String[] moreTest = new String[5];
+    ArrayList<String> arrayDate = new ArrayList<String>();
+    ArrayList<String> arrayDose = new ArrayList<String>();
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +45,8 @@ public class Test extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
 
         btnconvert = findViewById(R.id.btnconvert);
+        btnsave = findViewById(R.id.btnsave);
+        btnload = findViewById(R.id.btnload);
         original = findViewById(R.id.txtoriginal);
         changed = findViewById(R.id.txtchanged);
         test = findViewById(R.id.edittest);
@@ -59,60 +64,109 @@ public class Test extends AppCompatActivity {
         }
     }
 
-    public String LoadFile(View view){
-        String path = Environment.getExternalStorageDirectory().toString()+"/bluetooth"+ File.separator + "test.txt";
-        File file = new File(path);
-        StringBuilder sb = new StringBuilder();
+    public void convertHub(View view) {
 
-        if (file.isFile() && file.getPath().endsWith(".txt")) {
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                String line;
-                int i = 0;
-                while((line = br.readLine()) != null) {
-                    sb.append(line);
-                    moreTest[i] = line;
-                    i++;
-                }
-                test.setText(sb.toString());
-                waitingText = sb.toString();
-                br.close();
-                file.delete();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        load(view);
+        int dose = 0;
+        ArrayList<String> arrayQuery = new ArrayList<String>();
+
+        for (int j = 0; j<arrayDose.size(); j++){
+            dose = Integer.parseInt(arrayDose.get(j));
+            arrayQuery.add(convertDate(arrayDate.get(j)));
         }
-        else {
-            sb.append("LUL");
-        }
-        return sb.toString();
+
+        original.setText(String.valueOf(dose));
+        changed.setText(arrayQuery.get(arrayQuery.size()-1));
+
+        /*original.setText(String.valueOf(i/2));
+        changed.setText(String.valueOf(arrayDate.size()));
+        result.setText(String.valueOf(arrayDose.size()));*/
     }
 
+    public void save(View view) {
+        String text = test.getText().toString();
+        FileOutputStream fos = null;
 
+        try {
+            fos = openFileOutput(FILE_NAME_ONE, MODE_PRIVATE);
+            fos.write(text.getBytes());
 
-    public String convert(View view) {
+            test.getText().clear();
+            Toast.makeText(this,"Saved to " + getFilesDir() + "/" + FILE_NAME_ONE, Toast.LENGTH_LONG ).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void load(View view) {
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME_ONE);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            i = 0;
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+                if (i%2 == 0){
+                    arrayDate.add(text);
+                }
+                else {
+                    arrayDose.add(text);
+                }
+                i++;
+            }
+            test.setText(sb.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public int convertDoses(String dose) {
+
+        return Integer.parseInt(dose);
+    }
+
+    public String convertDate(String date) {
 
         String query = "";
-
-        //J'ai virer le loadfile pour tester plus simplement
-        moreTest[0] = test.getText().toString();
-
-
-        original.setText(moreTest[0]);
-        String[] arrayOfString = moreTest[0].split(" ", 0);
+        String[] arrayOfString = date.split(" ", 0);
         String[] year = arrayOfString[3].split(",", 0);
         query = query + year[0] + "-";//Year
 
-        Date date = null;//put your month name here
+        Date month = null;//put your month name here
         try {
-            date = new SimpleDateFormat("MMM", Locale.FRENCH).parse(arrayOfString[2]);
+            month = new SimpleDateFormat("MMM", Locale.FRENCH).parse(arrayOfString[2]);
         } catch (ParseException e) {
             e.printStackTrace();
         }
         Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
+        cal.setTime(month);
         int monthNumber=cal.get(Calendar.MONTH);
         String interMonth = Integer.toString(monthNumber);//A AJOUTER
         if(interMonth.length() == 1) {
@@ -121,6 +175,7 @@ public class Test extends AppCompatActivity {
         else {
             query = query + interMonth + "-";//Month//A CHANGER
         }
+        //query = query + Integer.toString(monthNumber) + "-";//Month
 
         if(arrayOfString[1].length() == 1) {
             query = query + "0" + arrayOfString[1] + " ";//Day
@@ -129,7 +184,6 @@ public class Test extends AppCompatActivity {
             query = query + arrayOfString[1] + " ";//Day//A CHANGER
         }
         query = query + arrayOfString[4];//hh:mm:ss
-        changed.setText(query);
 
         return query;
     }
